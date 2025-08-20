83f1ba589241560d85f51bb0dfa2cb631ef7b6e1
pedro emilio <potolino456@gmail.com>
2025-08-20 15:06:40 -0300

    avanco

---
+++ MauiAppFit/Helpers/SQLiteDatabaseHelper.cs
@@ -0,0 +1,67 @@
+﻿
+using MauiAppFit.Models;
+using MauiAppFit.Views;
+using SQLite;
+namespace MauiAppFit.Helpers
+
+{
+    public class SQLiteDataBaseHelper
+    {
+
+        readonly SQLiteAsyncConnection _db;
+
+        public SQLiteDataBaseHelper(string dbPath)
+        {
+            _db = new SQLiteAsyncConnection(dbPath);
+            _db.CreateTableAsync<Atividade>().Wait();
+        }
+
+        public Task<List<Atividade>> GetAllRows()
+        {
+            return _db.Table<Atividade>().OrderByDescending(i => i.Id).ToListAsync();
+        }
+
+        public Task<Atividade> GetById(int id)
+        {
+            return _db.Table<Atividade>().FirstAsync(i => i.Id == id);
+        }
+
+        public Task<int> Insert(Atividade model)
+        {
+            return _db.InsertAsync(model);
+        }
+
+        public Task<List<Atividade>> Update(Atividade model)
+        {
+            string sql = "UPDATE Atividades SET Descricao = ?, Data=?, Page=?, Observacoes = ? WHERE Id = ?";
+
+            return _db.QueryAsync<Atividade>
+                (
+                  sql,
+                  model.Descricao,
+                  model.Data,
+                  model.Peso,
+                  model.Observacoes,
+                  model.Id
+                );
+        }
+
+        public Task<int> Delete(int id)
+        {
+            return _db.Table<Atividade>().DeleteAsync(i => i.Id == id);
+        }
+
+        public Task<List<Atividade>> Search(string q)
+        {
+            string sql = "SELECT * FROM Atividades " +
+                "WHERE Descricao LIKE '%'" + q + "'%'";
+
+            return _db.QueryAsync<Atividade>(sql);
+
+        }
+
+
+    }
+}
+
+
